34cbd4ae27a57cae87b8929e0be9c2f03dd3faaf
Vikaskaise <152466052+Vikaskaise@users.noreply.github.com>
2023-12-15 16:10:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,12 +18,12 @@ def is_enabled(value, default):
 
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '20389440'))
-API_HASH = environ.get('API_HASH', 'a1a06a18eb9153e9dbd447cfd5da2457')
+API_ID = int(environ.get('API_ID', '24774840'))
+API_HASH = environ.get('API_HASH', '80911fee6a41631e03147b357ed64e69')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
-CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 PICS = (environ.get('PICS', 'https://telegra.ph/file/b3ea5679a504ac613d350.jpg https://telegra.ph/file/2d67786b341b5af70e6cf.jpg https://telegra.ph/file/b63750ea539b50ab04f90.jpg https://telegra.ph/file/2c5a543b1fc2f7fe7d697.jpg https://telegra.ph/file/441071ce20e07ad19d9b5.jpg https://telegra.ph/file/2242d4b54a6e384f22686.jpg https://telegra.ph/file/47ed99f4c51d20fa889b2.jpg')).split() #SAMPLE PIC
@@ -33,7 +33,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6168162777').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001905931730').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001856885647 -1001818726355 -1001928747063').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
@@ -41,8 +41,8 @@ auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '')
-reqst_channel = environ.get('REQST_CHANNEL_ID', '')
+support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002134939566')
+reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002031701693')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
@@ -57,16 +57,16 @@ VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'ziplinker.net')
 SHORTLINK_API = environ.get('SHORTLINK_API', '37752ccfafb8030f3614dd384405293d5a629203')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
-DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
+DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1002127830802').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 PORT = environ.get("PORT", "8080")
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/searchinggroupmovie')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/newrelease1234')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/PlayJustMovie')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/newmoviedownload1')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+V9cxgu5b5oi9NGZf')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/requestmovie12345')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'Hello My Dear Friends ❤️')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001623633000'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001167930180'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'yoursupport12')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
@@ -80,7 +80,7 @@ LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False")
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
+FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-1001856885647')).split()]
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
@@ -105,8 +105,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://movierequestfree-a59484e5d0e2.herokuapp.com".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://movierequestfree-a59484e5d0e2.herokuapp.com/".format(FQDN, PORT)
+URL = "https://illegal-filter-bot-f33ec61d18ce.herokuapp.com".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://illegal-filter-bot-f33ec61d18ce.herokuapp.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
@@ -121,9 +121,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',False))
 if HAS_SSL:
-    URL = "https://movierequestfree-a59484e5d0e2.herokuapp.com/".format(FQDN)
+    URL = "https://illegal-filter-bot-f33ec61d18ce.herokuapp.com/".format(FQDN)
 else:
-    URL = "https://movierequestfree-a59484e5d0e2.herokuapp.com/".format(FQDN)
+    URL = "https://illegal-filter-bot-f33ec61d18ce.herokuapp.com/".format(FQDN)
 
 
 
